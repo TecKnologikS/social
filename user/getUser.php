69a74fba5acd2fca48329c9a83e4b57ed1f147c1
TecKnologikS <tecknologiksdev@gmail.com>
2015-05-19 15:12:03 +0200

    dispatch dans different fichier

--- user/getUser.php
+++ user/getUser.php
@@ -1,5 +1,6 @@
 <?php
 	include("user.php");
+	include("UserDAO.php");
 	if (isset($_GET["id"]))	{
 		echo json_encode(getUserByID($_GET["id"]));
 	}
@@ -12,17 +13,7 @@
 
  	function getUserByID($id)
         {
-		$user = new User();
-		$bdd = new PDO('mysql:host=localhost;dbname=socialnet', 'root', 'root');
-		$resultats = $bdd->query("SELECT * FROM user WHERE ID=".$id);
-		$row = $resultats->fetch();
-		if ($row != null)
-		{
-			$user->id = $row["id"];
-			$user->name = $row["name"];
-			$user->first_name = $row["first"];
-		}
-                return $user;
+		return getUser(" id=".$id);
         }
 
 	function getUserByName($name)
